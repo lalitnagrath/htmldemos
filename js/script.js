aba190eb14c58e3a2f447686753bf01587094468
freelancer <lalitnagrath@gmail.com>
2021-12-17 20:11:09 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var num = 10;
+var string = 'hello there';
+var isRad =true ;
+
+if(num == 10){
+    console.log("yeash buddy")
+}
+else{
+    console.log("nopes");
+}
+document.getElementById('box').innerHTML=num+10;
