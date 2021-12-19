8f3fb86c06f1ab985c65923fe731d959ffcb97d0
freelancer <lalitnagrath@gmail.com>
2021-12-19 23:31:00 +0530

    commit

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,7 @@
 var groceries =["milk","Eggs","Bread"];
 
 function listGroceries(){
-    for(var x=0;x<=2;x++){
+    for(var x=0;x<groceries.length;x++){
         console.log(groceries[x]);
     }
 }
