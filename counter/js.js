c0190cfeb144cca0f9b8aea6b4d0bdafbdea6bd7
freelancer <lalitnagrath@gmail.com>
2021-12-21 23:47:38 +0530

    done

--- counter/js.js
+++ counter/js.js
@@ -1,7 +1,2 @@
-let header = document.querySelector(".header");
 
-console.log(header);
-
-let header1 = document.querySelectorAll("header");
-
-console.log(header1);
+document.getElementById("heading").innerHTML ="Hello Bros";
